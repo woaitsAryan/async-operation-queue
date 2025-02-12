01f5e2bb7ba0ca053ee466a60f5f0609fe4cb404
woaitsAryan <yoprince.ab@gmail.com>
2025-02-12 08:34:18 +0000

    feat: combined types in a single export

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ import type { BuildConfig } from 'bun'
 import dts from 'bun-plugin-dts'
 
 const defaultBuildConfig: BuildConfig = {
-  entrypoints: ['./src/index.ts', './src/types.ts'],
+  entrypoints: ['./src/index.ts'],
   outdir: './dist',
   target: 'node',
   minify: true
